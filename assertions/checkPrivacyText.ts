58a7f8a3e4def078d430b0f5985730e58291c464
Smile-TA <smile.bestybay@thomasarts.com>
2024-11-08 08:52:41 -0800

    add relative link check

--- assertions/checkPrivacyText.ts
+++ assertions/checkPrivacyText.ts
@@ -4,8 +4,15 @@ const textBody =
   "Any information that we collect when you opt into receiving SMS and MMS text messages from us is used solely for the management of such texting communications program.  We do not transfer your consent to be contacted via the texting program to any other entity.";
 
 export async function checkPrivacyText(page: Page) {
-  await expect
-    .soft(page.getByText("INFORMATION COLLECTED FOR TEXTING PROGRAM"))
-    .toBeVisible();
+  await expect(
+    page.getByRole("link", {
+      name: "Information Collected for Texting Program",
+    })
+  ).toHaveAttribute("href", "#texting");
+  const h2 = page.getByRole("heading", {
+    name: "INFORMATION COLLECTED FOR TEXTING PROGRAM",
+  });
+  await expect.soft(h2).toBeVisible();
+  await expect.soft(h2).toHaveId("texting");
   await expect.soft(page.getByText(textBody)).toBeVisible();
 }
