a05c06c8a8775395bf29316431b2034c799d02ff
Smile-TA <smile.bestybay@thomasarts.com>
2024-12-12 14:06:40 -0800

    add source code from strip-ansi package because dynamic import is broken on pw

--- axe-summary-reporter.ts
+++ axe-summary-reporter.ts
@@ -18,8 +18,8 @@ class AxeSummaryReporter implements Reporter {
 
   onTestEnd(test: TestCase, result: TestResult) {
     console.log(`Finished test ${test.title}: ${result.status}`);
-    const messageString = result.error?.message;
-    console.log(messageString);
+    const messageString = result.error?.message ?? "";
+    console.log(stripAnsi(messageString));
   }
 
   onEnd(result: FullResult) {
@@ -28,3 +28,22 @@ class AxeSummaryReporter implements Reporter {
 }
 
 export default AxeSummaryReporter;
+
+// node_modules/ansi-regex/index.js
+function ansiRegex({ onlyFirst = false } = {}) {
+  const ST = "(?:\\u0007|\\u001B\\u005C|\\u009C)";
+  const pattern = [
+    `[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?${ST})`,
+    "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))",
+  ].join("|");
+  return new RegExp(pattern, onlyFirst ? void 0 : "g");
+}
+
+// node_modules/strip-ansi/index.js
+const regex = ansiRegex();
+function stripAnsi(string: string) {
+  if (typeof string !== "string") {
+    throw new TypeError(`Expected a \`string\`, got \`${typeof string}\``);
+  }
+  return string.replace(regex, "");
+}
