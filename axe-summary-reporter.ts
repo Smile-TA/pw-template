43270d40f720a361033bf096f80d19a0830f54d0
Smile-TA <smile.bestybay@thomasarts.com>
2025-02-17 15:40:02 -0800

    clean up comments, refactor getViolation to be a pure function

--- axe-summary-reporter.ts
+++ axe-summary-reporter.ts
@@ -24,21 +24,6 @@ class AxeSummaryReporter implements Reporter {
 
   onTestEnd(test: TestCase, result: TestResult) {
     console.log(`Finished test ${test.title}: ${result.status}`);
-    // const messageString = result.error?.message ?? "";
-    // console.log(stripAnsi(messageString));
-    if (test.title.includes("WCAG")) {
-      // const violations = result.attachments.filter(
-      //   (attachment) => attachment.name === "violations"
-      // );
-      // const s = violations[0].body?.toString("utf-8") ?? "";
-      // console.log(
-      //   `TEST RESULST FOR ${test.title}
-      //   `,
-      //   JSON.stringify(JSON.parse(s), null, "    "),
-      //   `
-      //   END TEST RESULST FOR ${test.title}`
-      // );
-    }
   }
 
   onEnd(result: FullResult) {
@@ -46,9 +31,7 @@ class AxeSummaryReporter implements Reporter {
     // Try to use the build pattern from the html reporter
     // https://github.com/microsoft/playwright/blob/main/packages/playwright/src/reporters/html.ts#L241
     const suites = this.suite;
-    const violations: any = [];
-    this.getViolations(suites, violations);
-    const violationsDecoded = violations
+    const violationsDecoded = this.getViolations(suites)
       .map((violation: { body: { toString: (arg0: string) => any } }) =>
         JSON.parse(violation.body.toString("utf-8"))
       )
@@ -63,7 +46,7 @@ class AxeSummaryReporter implements Reporter {
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  getViolations(suite: Suite | TestCase, res?: any) {
+  getViolations(suite: Suite | TestCase, res: any = []) {
     if (suite.type === "test") {
       suite.results.forEach((result) => {
         res?.push(
@@ -75,26 +58,8 @@ class AxeSummaryReporter implements Reporter {
     } else {
       suite.entries().forEach((s) => this.getViolations(s, res));
     }
+    return res;
   }
 }
 
 export default AxeSummaryReporter;
-// Leach ansi strip here just in case I might need it later.
-// // node_modules/ansi-regex/index.js
-// function ansiRegex({ onlyFirst = false } = {}) {
-//   const ST = "(?:\\u0007|\\u001B\\u005C|\\u009C)";
-//   const pattern = [
-//     `[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?${ST})`,
-//     "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))",
-//   ].join("|");
-//   return new RegExp(pattern, onlyFirst ? void 0 : "g");
-// }
-
-// // node_modules/strip-ansi/index.js
-// const regex = ansiRegex();
-// function stripAnsi(string: string) {
-//   if (typeof string !== "string") {
-//     throw new TypeError(`Expected a \`string\`, got \`${typeof string}\``);
-//   }
-//   return string.replace(regex, "");
-// }
