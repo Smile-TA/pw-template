cf9ccdc29f3cd8679a56262c2ccf809bb5e2d400
Smile-TA <smile.bestybay@thomasarts.com>
2025-04-03 14:09:26 -0700

    add best practice tag to the wcag test

--- assertions/checkWCAG.ts
+++ assertions/checkWCAG.ts
@@ -4,7 +4,14 @@ import { createHtmlReport } from "axe-html-reporter";
 
 export async function checkWCAG(page: Page, testInfo: TestInfo) {
   const accessibilityScanResults = await new AxeBuilder({ page })
-    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22aa"])
+    .withTags([
+      "wcag2a",
+      "wcag2aa",
+      "wcag21a",
+      "wcag21aa",
+      "wcag22aa",
+      "best-practice",
+    ])
     .analyze();
 
   const reportHTML = createHtmlReport({
