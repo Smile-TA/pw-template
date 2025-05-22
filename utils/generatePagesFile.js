935f8c1e5a114ae096f72d6fd1107bccdd4de870
Smile-TA <smile.bestybay@thomasarts.com>
2025-05-22 12:53:28 -0700

    add planenroll condition to generate pages file util

--- utils/generatePagesFile.js
+++ utils/generatePagesFile.js
@@ -5,6 +5,10 @@ dotenv.config();
 const url = process.env.BASE_URL;
 if (url === "undefined") {
   throw Error("Missing base url");
+} else if (url.includes("planenroll")) {
+  fs.writeFile("./pages.ts", 'export const pages = ["/", "/thanks"];', (e) =>
+    console.log(e)
+  );
 } else {
   getPageLinks(url, "pages").then((res) =>
     fs.writeFile(
