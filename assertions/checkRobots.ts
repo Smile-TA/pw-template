a89ff332aac51d8ad1b3ba4d0d8d43da9da1cf84
Smile-TA <smile.bestybay@thomasarts.com>
2025-05-29 14:16:34 -0700

    update check robots with better regex

--- assertions/checkRobots.ts
+++ assertions/checkRobots.ts
@@ -4,8 +4,18 @@ export async function checkRobots(page: Page) {
   const meta = page.locator('meta[name*="robots"]');
   const isStaging = page.url().includes("staging");
   if (isStaging) {
-    await expect.soft(meta).toHaveAttribute("content", "noindex, nofollow");
+    await expect
+      .soft(meta)
+      .toHaveAttribute(
+        "content",
+        /\bnoindex\b.*\bnofollow\b|\bnofollow\b.*\bnoindex\b/
+      );
   } else {
-    await expect.soft(meta).not.toHaveAttribute("content", "noindex, nofollow");
+    await expect
+      .soft(meta)
+      .not.toHaveAttribute(
+        "content",
+        /\bnoindex\b.*\bnofollow\b|\bnofollow\b.*\bnoindex\b/
+      );
   }
 }
