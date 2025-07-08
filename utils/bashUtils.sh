2a9f8d75630ff4d7591c7d51a679841ac4578d0f
Smile-TA <smile.bestybay@thomasarts.com>
2025-07-08 08:50:57 -0700

    update bash utils

--- utils/bashUtils.sh
+++ utils/bashUtils.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Converts input json array into markdown bullet list. Useful when pasting as markdown into Google docs.
-echo 'arr' | jq -r '.[] | "- Page \(.pageSrc) has images with large file sizes at these file sources:\n" + (.fileSrc | map("  - " + .) | join("\n"))' | pbcopy
+echo 'INPUT_ARRAY' | jq -r '.[] | "- \(. )"' | pbcopy
+
+# Converts image file sources with large file sizes into markdown bullet list.
+cat ./summaries/image-summary.json | jq -r '.[] | "- Page \(.pageSrc) has images with large file sizes at these file sources:\n" + (.fileSrc | map("  - \(.url) (\(.size) kB)") | join("\n"))' | pbcopy
