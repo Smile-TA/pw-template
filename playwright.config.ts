9e60828e202394b42a1fea84f01f1981b9491b83
Smile-TA <smile.bestybay@thomasarts.com>
2025-08-19 10:51:41 -0700

    parametarize slow mo

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,6 +36,9 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL,
+    launchOptions: {
+      slowMo: parseInt(process.env.SLOW_MO ?? "0"),
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "retain-on-failure",
