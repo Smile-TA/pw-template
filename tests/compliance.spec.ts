f25c709eeb99e4233db3620def8f408d937a8b65
Smile-TA <smile.bestybay@thomasarts.com>
2026-01-05 20:30:47 -0800

    implement the rest of single page checks into the compliance project

--- tests/compliance.spec.ts
+++ tests/compliance.spec.ts
@@ -1,7 +1,34 @@
-import { test } from "@playwright/test";
-import { checkCookieYes } from "../assertions/checkCookieYes";
+import { test, type Page } from "@playwright/test";
+import { allDomains } from "../all-domains";
 
-test("check cookie consent", async ({ page }) => {
-  await page.goto("");
-  await checkCookieYes(page);
+import { checkCookieYes } from "../assertions/checkCookieYes";
+import { checkLegalLinks } from "../assertions/checkLegalLinks";
+import { checkGTM } from "../assertions/checkGTM";
+
+allDomains.forEach((d) => {
+  test.describe(`test ${d} domain`, () => {
+    /**
+     * @see {@link https://playwright.dev/docs/test-retries#reuse-single-page-between-tests Playwright doc reference}
+     */
+    let page: Page;
+
+    test.beforeAll(async ({ browser }) => {
+      page = await browser.newPage();
+      await page.goto(d);
+    });
+
+    test.afterAll(async () => {
+      await page.close();
+    });
+
+    test("check cookie consent", async ({}) => {
+      await checkCookieYes(page);
+    });
+    test("check legal links", async () => {
+      await checkLegalLinks(page);
+    });
+    test("Check GTM", async ({}) => {
+      await checkGTM(page);
+    });
+  });
 });
