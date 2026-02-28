e2e2bf83c137550a62b65c9df1b112268495ea8e
Smile-TA <smile.bestybay@thomasarts.com>
2026-02-27 18:22:32 -0800

    add aio migration extensions to the plugin check

--- assertions/checkPlugins.ts
+++ assertions/checkPlugins.ts
@@ -5,6 +5,10 @@ export async function checkPlugins(page: Page) {
   const requiredPlugins = [
     "Yoast SEO",
     "All-in-One WP Migration and Backup",
+    [
+      "All-in-One WP Migration URL Extension",
+      "All-in-One WP Migration Unlimited Extension",
+    ],
     "Wordfence Security",
   ];
 
@@ -12,8 +16,20 @@ export async function checkPlugins(page: Page) {
   await page.goto("/wp-admin/plugins.php");
   const pluginTitleCell = page.locator("td.plugin-title strong");
   for (const plugin of requiredPlugins) {
-    await expect
-      .soft(page.getByText(plugin, { exact: true }).and(pluginTitleCell))
-      .toBeVisible();
+    if (Array.isArray(plugin)) {
+      await expect
+        .soft(
+          page
+            .getByText(plugin[0], { exact: true })
+            .or(page.getByText(plugin[1], { exact: true }))
+            .first()
+            .and(pluginTitleCell),
+        )
+        .toBeVisible();
+    } else if (typeof plugin === "string") {
+      await expect
+        .soft(page.getByText(plugin, { exact: true }).and(pluginTitleCell))
+        .toBeVisible();
+    }
   }
 }
